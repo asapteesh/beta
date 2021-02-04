9ac5aad44e1ba570a6e46daf18f1df6231310693
Franklin Waller <fr.l.waller@gmail.com>
2021-02-04 09:32:11 +0100

    fix(ga): Fix issue where pageview where not coming through

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,23 @@
 import React, { ReactElement } from 'react';
 
-import { Route, Switch } from 'react-router';
+import { Route, Switch, useHistory } from 'react-router';
 
 import { routes } from './routes';
 import { themeContext, useDarkMode } from './utils/hooks/useDarkModeTheme';
 
 function App(): ReactElement {
     const [theme, toggleTheme] = useDarkMode();
+    const history = useHistory();
+
+    history.listen((currentLocation) => {
+        // @ts-ignore
+        if (window && window.gtag) {
+            // @ts-ignore
+            window.gtag('config', process.env.REACT_APP_GA_ID, {
+                'page_path': currentLocation.pathname + currentLocation.search,
+            });
+        }
+    });
 
     return (
         // @ts-ignore

10c85274af4d5dcd550cee596e8d3b121c67ea53
Franklin Waller <fr.l.waller@gmail.com>
2021-02-15 16:24:48 +0100

    chore(near): Only use FluxSdk for connections

--- src/services/WalletService.ts
+++ src/services/WalletService.ts
@@ -1,75 +1,11 @@
 import {
     Near,
-    connect,
     WalletConnection,
-    keyStores,
 } from "near-api-js";
+
 import FluxSdk from '@fluxprotocol/amm-sdk';
 
-import { NETWORK, NODE_ENV, NULL_CONTRACT } from '../config';
-
-export interface ConnectConfig {
-    accountId?: string;
-    nearInstance?: Near,
-    walletInstance?: WalletConnection,
-    customNodeUrl?: string
-}
-
-interface NetworkConfig {
-    networkId: string,
-    nodeUrl: string,
-    contractName: null,
-    walletUrl: string,
-    initialBalance: string
-}
-
-export function getNetworkConfig(networkName: string, nodeUrl?: string): NetworkConfig {
-    let network: NetworkConfig;
-
-    switch (networkName) {
-        default:
-            network = {
-                networkId: 'testnet',
-                nodeUrl: nodeUrl || 'https://rpc.testnet.near.org',
-                contractName: null,
-                walletUrl: 'https://wallet.testnet.near.org',
-                initialBalance: "100000000"
-            };
-    }
-
-    return network;
-}
-
-let nearInstance: Near;
-
-export async function connectNear() {
-    if (nearInstance) {
-        return nearInstance;
-    }
-
-    const networkConfig = getNetworkConfig('testnet');
-    nearInstance = await connect({
-        ...networkConfig,
-        deps: {
-            keyStore: new keyStores.BrowserLocalStorageKeyStore(),
-        }
-    });
-
-    return nearInstance;
-}
-
-let walletConnection: WalletConnection;
-
-export async function connectWallet(): Promise<WalletConnection> {
-    if (walletConnection) {
-        return walletConnection;
-    }
-
-    const near = await connectNear();
-
-    walletConnection = new WalletConnection(near, NULL_CONTRACT);
-    return walletConnection;
-}
+import { NETWORK } from '../config';
 
 let sdkInstance = new FluxSdk({
     network: NETWORK,
@@ -79,3 +15,17 @@ export async function connectSdk(): Promise<FluxSdk> {
     await sdkInstance.connect();
     return sdkInstance;
 }
+
+export async function connectNear(): Promise<Near> {
+    const sdk = await connectSdk();
+    return sdk.near!;
+}
+
+export async function connectWallet(): Promise<WalletConnection> {
+    const sdk = await connectSdk();
+    return sdk.walletConnection!;
+}
+
+
+
+

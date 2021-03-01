2954b334d9bde8776eb7124187a21c29d45661a7
jasperdg <jasperdegooijer@gmail.com>
2021-03-01 14:31:01 +0100

    added validity escrow view to swap overview

--- src/containers/TokenSwapper/components/SwapOverview/SwapOverview.tsx
+++ src/containers/TokenSwapper/components/SwapOverview/SwapOverview.tsx
@@ -5,6 +5,8 @@ import trans from '../../../../translation/trans';
 import Overview from "./../../../../components/Overview";
 import mutateFormValues from './utils/overviewMutation';
 import Big from 'big.js';
+import { formatCollateralToken } from '../../../../services/CollateralTokenService';
+import { toToken } from '@fluxprotocol/amm-sdk/dist/core/FluxSdkUtils';
 
 interface SwapOverviewProps {
     formValues: SwapFormValues
@@ -43,6 +45,47 @@ export default function SwapOverview({formValues}: SwapOverviewProps): ReactElem
             key: trans('market.overview.maxPayout'),
             value: `${formValues.formattedAmountOut || "0"} ${formValues.fromToken.tokenSymbol} (+${profitPercentage}%)`
         })
+    } else if (amountIn.gt("0")) {
+        const spent = new Big(formValues.fromToken.spent);
+        const balance = new Big(formValues.fromToken.balance);
+        const avgPaidPrice = balance.gt("0") ? spent.div(balance) : new Big("0");
+        const avgSellPrice = amountIn.gt("0") ? amountOut.div(amountIn) : new Big("0");
+        const spendAtAvgSellPrice = avgSellPrice.mul(amountIn);
+        const spendAtAvgPaidPrice = avgPaidPrice.mul(amountIn);
+
+        if (avgPaidPrice.gt(avgSellPrice)) {
+            // Escrow invalid
+            const escrowInvalid = spendAtAvgPaidPrice.minus(spendAtAvgSellPrice);
+            const formattedEscrowInvalid = escrowInvalid.gt("0.01") ? formatCollateralToken(escrowInvalid.toString(), formValues.toToken.decimals) : "< 0.01";
+            overViewData.push(
+                {
+                    key: trans('market.overview.escrowInvalid'),
+                    value: `~${formattedEscrowInvalid}${formValues.toToken.tokenSymbol}`
+                },
+                {
+                    key: trans('market.overview.paidNow'),
+                    value: `${formValues.formattedAmountOut} ${formValues.toToken.tokenSymbol}`
+                }
+            );
+ 
+        } else if ((avgSellPrice.gt(avgPaidPrice))){
+            // Escrow valid
+            const escrowInvalid = spendAtAvgSellPrice.minus(spendAtAvgPaidPrice);
+            const formattedEscrowInvalid = escrowInvalid.gt("0.01") ? formatCollateralToken(escrowInvalid.toString(), formValues.toToken.decimals) : "< 0.01";
+            const paidNow = amountOut.minus(escrowInvalid);
+            const formattedOutNow = formatCollateralToken(paidNow.toString(), formValues.toToken.decimals);
+
+            overViewData.push(...[
+                {
+                    key: trans('market.overview.escrowValid'),
+                    value: `~${formattedEscrowInvalid} ${formValues.toToken.tokenSymbol}`
+                },
+                {
+                    key: trans('market.overview.paidNow'),
+                    value: `${formattedOutNow} ${formValues.toToken.tokenSymbol}`
+                }
+            ]);
+        }
     }
 
     return <Overview data={overViewData} header={trans('market.label.overview', {}, true)} />;
